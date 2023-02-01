9e17e6d17977f354a804ecb56b7b95dbb2f3805e
Bek <nematjanov95@gmail.com>
2023-02-02 00:01:03 +0500

    fixing

--- lib/view/pages/home/all_products.dart
+++ lib/view/pages/home/all_products.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import '../../../controller/home_controller.dart';
+import '../../components/cached_network_image.dart';
 import '../../components/custom_textform.dart';
+import '../../style/style.dart';
 
 class ProductListPage extends StatefulWidget {
   const ProductListPage({Key? key}) : super(key: key);
@@ -101,34 +104,78 @@ class _ProductListPageState extends State<ProductListPage> {
                         context.watch<HomeController>().listOfProduct.length,
                     itemBuilder: (context, index) {
                       return Container(
-                        margin: EdgeInsets.all(16),
-                        width: double.infinity,
-                        height: 90,
-                        color: Colors.pinkAccent,
+                        margin: const EdgeInsets.only(bottom: 30),
+                        height: 88.h,
+                        width: 380.w,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 50,
+                                  offset: const Offset(0, 6),
+                                  color:
+                                      const Color(0xff5A6CEA).withOpacity(0.08))
+                            ],
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(16)),
+                            color: Colors.white),
                         child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            state.listOfProduct[index].image == null
-                                ? const SizedBox.shrink()
-                                : Image.network(
-                                    state.listOfProduct[index].image ?? "",
-                                    height: 64,
-                                    width: 64,
-                                  ),
+                            12.horizontalSpace,
+                            Padding(
+                                padding: const EdgeInsets.only(top: 12),
+                                child: context
+                                            .watch<HomeController>()
+                                            .listOfProduct[index]
+                                            .image ==
+                                        null
+                                    ? const SizedBox.shrink()
+                                    : CustomImageNetwork(
+                                        image: context
+                                                .watch<HomeController>()
+                                                .listOfProduct[index]
+                                                .image ??
+                                            "",
+                                        height: 64.h,
+                                        width: 64.w,
+                                      )),
+                            20.horizontalSpace,
                             Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(state.listOfProduct[index].name ?? ""),
-                                Text(state.listOfProduct[index].desc ?? ""),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 5),
+                                  child: Text(
+                                      context
+                                              .watch<HomeController>()
+                                              .listOfProduct[index]
+                                              .name ??
+                                          "",
+                                      style: Style.textStyleRegular()),
+                                ),
+                                4.verticalSpace,
+                                Text(
+                                    context
+                                            .watch<HomeController>()
+                                            .listOfProduct[index]
+                                            .desc ??
+                                        "",
+                                    style: Style.textStyleRegular2(
+                                        size: 14,
+                                        textColor: const Color(0xff858C94)))
                               ],
                             ),
                             const Spacer(),
-                            Text(state.listOfProduct[index].price.toString()),
-                            IconButton(
-                                onPressed: () {
-                                  event.changeLike(index);
-                                },
-                                icon: (state.listOfProduct[index].isLike)
-                                    ? Icon(Icons.favorite)
-                                    : Icon(Icons.favorite_border))
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 32, top: 22),
+                              child: Text(
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  '\$${context.watch<HomeController>().listOfProduct[index].price.toString()}',
+                                  style: Style.textStyleRegular(
+                                      size: 20, textColor: Style.primaryColor)),
+                            )
                           ],
                         ),
                       );
