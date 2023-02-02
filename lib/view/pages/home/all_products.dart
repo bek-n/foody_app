67691751abed1ccb95368d71a081169b7d326317
Bek <nematjanov95@gmail.com>
2023-02-02 22:21:37 +0500

    fixed some bugs

--- lib/view/pages/home/all_products.dart
+++ lib/view/pages/home/all_products.dart
@@ -1,4 +1,3 @@
-import 'package:animated_react_button/animated_react_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
