5de3c3d13facd042f98b9b0db22357517a8412a3
Bek <nematjanov95@gmail.com>
2023-02-02 22:53:07 +0500

    added chtas model

---
+++ lib/model/chats_model.dart
@@ -0,0 +1,30 @@
+
+import 'package:foody_app/model/user_model.dart';
+
+class ChatsModel {
+  final List ids;
+  final UserModel sender;
+  final bool userStatus;
+  final UserModel resender;
+
+  ChatsModel({
+    required this.userStatus,
+    required this.ids,
+    required this.sender,
+    required this.resender,
+  });
+
+  factory ChatsModel.fromJson({
+    required Map data,
+    Map<String, dynamic>? sender,
+    Map<String, dynamic>? resender,
+    required bool isOnline,
+  }) {
+    return ChatsModel(
+      ids: data["ids"],
+      sender: UserModel.fromJson(sender),
+      resender: UserModel.fromJson(resender),
+      userStatus: isOnline,
+    );
+  }
+}
